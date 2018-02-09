8fd472bcaf05910db52b5ea69b875c81f5eac3e2
Nuttapon Pattanavijit <nut@jitta.com>
2018-02-09 22:04:18 +0700

    Remove tmp file

--- graphql-02-apollo/tmp.js
+++
@@ -1,9 +0,0 @@
-const Post = require('./models/Post')
-const Comment = require('./models/Comment')
-;(async () => {
-  // await Comment.create(4, 1, 'comment 1')
-  // await Comment.create(5, 1, 'comment 2')
-  // await Comment.create(4, 2, 'comment 3')
-  // await Comment.create(5, 2, 'comment 4')
-  console.log(await Comment.listByPost(1))
-})()
